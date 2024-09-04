a693ae3fbdac4b9b3d40eedb63fb48aba03e4f96
VigilLight <VigilLight@DESKTOP-BTRJ6SU>
2024-09-05 01:30:16 +0800

    Add project files.

---
+++ Location_Autocomplete/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows.Forms;
+
+namespace Location_Autocomplete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new Location_Autocomplete.AutoCompleteTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input location";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Values = null;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 269);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Location Autocomplete";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private AutoCompleteTextBox textBox1;
+    }
+}
+
---
+++ Location_Autocomplete/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Nancy.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Location_Autocomplete
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(() =>
+            {
+                this.Invoke(new Action(() =>
+                {
+                    List<string> data = new List<string>();
+                    if (!string.IsNullOrEmpty(textBox1.Text))
+                    {
+                        string strUrl = string.Format($@"https://api.geoapify.com/v1/geocode/autocomplete?text={textBox1.Text}&apiKey=55ce3f0485d441ae914ba6a9dbcddb8f");
+                        RestClient client = new RestClient(strUrl);
+                        RestRequest request = new RestRequest();
+                        string strReturn = client.ExecuteGet(request).Content;
+                        JavaScriptSerializer jss = new JavaScriptSerializer();
+                        dynamic response = jss.Deserialize<dynamic>(strReturn);
+                        if (response != null)
+                        {
+                            if (response["features"] != null)
+                            {
+                                dynamic results = response["features"];
+                                foreach (dynamic result in results)
+                                {
+                                    data.Add(result["properties"]["formatted"].ToString());
+                                }
+                                textBox1.Values = data.ToArray();
+                            }
+                        }
+                    }
+                }));
+            }));
+            th.Start();
+        }
+    }
+}
